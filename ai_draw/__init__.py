1563d454fa9f37ca4042fa96d3ef4108da046c5d
nullqwertyuiop <49110142+nullqwertyuiop@users.noreply.github.com>
2022-10-29 23:31:59 +0800

    :sparkles: Add `ai_draw`

---
+++ ai_draw/__init__.py
@@ -0,0 +1,67 @@
+import random
+
+from graia.ariadne import Ariadne
+from graia.ariadne.event.message import GroupMessage, FriendMessage, MessageEvent
+from graia.ariadne.message.chain import MessageChain
+from graia.ariadne.message.parser.twilight import (
+    Twilight,
+    FullMatch,
+    WildcardMatch,
+    RegexResult,
+)
+from graia.ariadne.util.saya import listen, dispatch, decorate
+from graia.saya import Channel
+
+import module.ai_draw.util
+from library.decorator.blacklist import Blacklist
+from library.decorator.distribute import Distribution
+from library.decorator.permission import Permission
+from library.decorator.switch import Switch
+from library.model.permission import UserPerm
+from library.util.dispatcher import PrefixMatch
+from library.util.message import send_message
+from module.ai_draw.util import render
+
+channel = Channel.current()
+
+
+@listen(GroupMessage, FriendMessage)
+@dispatch(Twilight(PrefixMatch(), FullMatch("生成图片"), WildcardMatch() @ "content"))
+@decorate(Switch.check(channel.module), Distribution.distribute(), Blacklist.check())
+async def sd_webui_generate(app: Ariadne, event: MessageEvent, content: RegexResult):
+    content: str = content.result.display
+    field = int(event.sender.group) if isinstance(event, GroupMessage) else 0
+    supplicant = int(event.sender)
+    msg = await render(field, supplicant, content)
+    await send_message(event, msg, app.account)
+
+
+@listen(GroupMessage, FriendMessage)
+@dispatch(
+    Twilight(
+        PrefixMatch(),
+        FullMatch("设置"),
+        FullMatch("sd"),
+        FullMatch("链接"),
+        WildcardMatch() @ "content",
+    )
+)
+@decorate(
+    Switch.check(channel.module, show_log=True),
+    Distribution.distribute(),
+    Blacklist.check(),
+    Permission.require(UserPerm.BOT_ADMIN),
+)
+async def sd_webui_set_link(app: Ariadne, event: MessageEvent, content: RegexResult):
+    url: str = content.result.display
+    url = url.lstrip("http://").lstrip("https://").rstrip("/")
+    url = f"wss://{url}/queue/join"
+    module.sd_webui.util.SD_URL = url
+    await send_message(event, MessageChain(f"已设置为 {url}"), app.account)
+
+
+@listen(GroupMessage, FriendMessage)
+@dispatch(Twilight(PrefixMatch(), FullMatch("test"), WildcardMatch() @ "content"))
+@decorate(Switch.check(channel.module), Distribution.distribute(), Blacklist.check())
+async def sd_webui_generate(app: Ariadne, event: MessageEvent, content: RegexResult):
+    await send_message(event, MessageChain("test"), app.account)
---
+++ ai_draw/util.py
@@ -0,0 +1,115 @@
+import random
+import string
+from asyncio import Lock
+from datetime import datetime
+
+import aiohttp
+from graia.ariadne.message.chain import MessageChain
+from kayaku import create
+from loguru import logger
+
+from library.model.config.service.fastapi import FastAPIConfig
+from library.module.file_server.util import serve_file, get_link
+from library.util.orm import orm
+from module.ai_draw.table import StableDiffusionHistory
+
+lock = Lock()
+SD_URL: str = ""
+
+
+async def _communicate(positive: str, session: str) -> str:
+    async with lock:
+        logger.info(f"{session}: 取得锁")
+        try:
+            async with aiohttp.ClientSession() as s:
+                async with s.ws_connect(SD_URL) as ws:
+                    await ws.send_json(
+                        {
+                            "fn_index": 51,
+                            "data": [
+                                positive,  # Positive
+                                "",  # Negative
+                                "None",  # Style 1
+                                "None",  # Style 2
+                                20,  # Sampling steps
+                                "Euler a",  # Sampling method
+                                False,
+                                False,
+                                1,  # Batch count
+                                1,  # Batch size
+                                7,  # CFG Scale
+                                -1,  # Seed
+                                -1,
+                                0,
+                                0,
+                                0,
+                                False,
+                                512,  # Width
+                                512,  # Height
+                                False,
+                                0.7,
+                                0,
+                                0,
+                                "None",
+                                0.9,
+                                5,
+                                "0.0001",
+                                False,
+                                "None",
+                                "",
+                                0.1,
+                                False,
+                                False,
+                                False,
+                                None,
+                                "",
+                                "Seed",
+                                "",
+                                "Nothing",
+                                "",
+                                True,
+                                False,
+                                False,
+                                None,
+                                "",
+                                "",
+                            ],
+                            "session_hash": session,
+                        }
+                    )
+                    while True:
+                        msg = await ws.receive_json()
+                        if msg["msg"] == "process_completed":
+                            logger.success(f"{session}: 生成完成")
+                            b64 = msg["output"]["data"][0][0]
+                            return f"<img src='{b64}'>"
+        except Exception as e:
+            logger.exception(f"{session}: 发生错误: {e}")
+            raise
+
+
+async def _serve(content: str, session: str) -> str:
+    return await serve_file(content.encode("utf-8"), f"{session}.html")
+
+
+async def _insert(field: int, supplicant: int, positive: str, uuid: str):
+    await orm.add(
+        StableDiffusionHistory,
+        time=datetime.now(),
+        field=field,
+        supplicant=supplicant,
+        positive=positive,
+        uuid=uuid,
+    )
+
+
+async def render(field: int, supplicant: int, positive: str) -> MessageChain:
+    session = "".join(random.choices(string.ascii_uppercase + string.digits, k=9))
+    try:
+        content = await _communicate(positive, session)
+    except Exception as e:  # noqa
+        # Already logged, just pass
+        return MessageChain('出现错误，可能是 SD 链接失效\n可使用 "[前缀]设置 sd 链接 <链接>" 重新设置')
+    uuid = await _serve(content, session)
+    await _insert(field, supplicant, positive, uuid)
+    return MessageChain(get_link(uuid))
